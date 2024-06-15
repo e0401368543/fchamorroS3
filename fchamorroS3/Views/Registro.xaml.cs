ac5c762b79e9839691995d62b65d9eb9da3ecec4
Freddy Chamorro <e0401368543@uisrael.edu.ec>
2024-06-14 21:06:27 -0500

    añadir Grid

--- fchamorroS3/Views/Registro.xaml.cs
+++ fchamorroS3/Views/Registro.xaml.cs
@@ -5,14 +5,23 @@ public partial class Registro : ContentPage
 	public Registro()
 	{
 		InitializeComponent();
-	}
+  
+
+    }
 
     private void btnGuardar_Clicked(object sender, EventArgs e)
     {
-		string usuario= txtUsuario.Text;
-		string contrasena= txtContrasena.Text;
-		DisplayAlert("Alerta", "Usuario Guardado", "Ok");
+        string usuario = txtUsuario.Text;
+        string contrasena = txtContrasena.Text;
+        DisplayAlert("Alerta", "Usuario Guardado", "Ok");
 		Navigation.PushAsync(new Login(usuario, contrasena));
 
+    }
+
+    private void btnCancelar_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Login());
+        txtUsuario.Text = string.Empty;
+        txtContrasena.Text = string.Empty;
     }
 }
